462df236837a68f24d91dc51fa6b20d3d2cea664
PavelChugreev <chugun92@gmail.com>
2020-11-26 22:07:03 +0300

    create scripts for change conditions and ranges

--- js/script.js
+++ js/script.js
@@ -1,19 +1,32 @@
 const push = document.querySelector(".push");
 const form = document.querySelector(".form");
-let number = 0;
+let itemsNum = document.querySelectorAll(".form-item").length;
 
-function createCondition(){
-    new Condition(number).render();
-}
-
+const range =   `<div class="inputs">
+                    от <input type="number" name="" id="">
+                    до <input type="number" name="" id="">
+                </div>`;
+const type =    `<div class="inputs">
+                    <select name="type" id="type">
+                        <option value="standert">Standart</option>
+                        <option value="silver">Silver</option>
+                        <option value="gold">Gold</option>
+                    </select>
+                </div>`;
+const status1 =  `<div class="inputs">
+                    <select name="status" id="status">
+                        <option value="standert">Активна</option>
+                        <option value="silver">Неактивна</option>
+                    </select>
+                </div>`;
 
 class Condition {
     constructor(num){
         this.num = num;
-
     }
     render() {
         const formItem = document.createElement("div");
+        let rangeNum = formItem.querySelectorAll(".range").length;
         formItem.classList.add("form-item");
         formItem.innerHTML = 
         `                    
@@ -30,6 +43,7 @@ class Condition {
             </div>
             <div class="item-buttons">
                 <div class="add-btn">
+                    <button class="hide"></button>
                 </div>
                 <div class="del-btn">
                     <button><i class="fas fa-trash-alt"></i>  Удалить условие</button>
@@ -37,16 +51,95 @@ class Condition {
             </div>
         `;
         form.append(formItem);
-        number++;
+        itemsNum++;
 
+        const addBtn = formItem.querySelector(".add-btn");
         const delBtn = formItem.querySelector(".del-btn");
-        delBtn.addEventListener("click", () =>{
+        const select = formItem.querySelector('select');
+
+        delBtn.addEventListener("click", () => {
             formItem.remove();
-            number--;
+            itemsNum--;
+            rangeNum = 0;
+
         });
+        
+        select.addEventListener("change", () => {
+            const rangeWrap = formItem.querySelector('.range-wrapper');
+            rangeWrap.innerHTML = "";
+            rangeNum = 0;
+            if(select.value == "age"){
+                createRange();
+                addBtn.removeEventListener("click", createType);
+                addBtn.removeEventListener("click", createStatus);
+                addBtn.addEventListener("click", createRange);
+            } 
+            else if (select.value == "type"){
+                createType();
+                addBtn.removeEventListener("click", createRange);
+                addBtn.removeEventListener("click", createStatus);
+                addBtn.addEventListener("click", createType);
+
+            } 
+            else if (select.value == "status"){              
+                createStatus();
+                addBtn.removeEventListener("click", createType);
+                addBtn.removeEventListener("click", createRange);
+                addBtn.addEventListener("click", createStatus);                
+            } 
+            else {
+                const btn = formItem.querySelector("button");
+                btn.classList.add("hide");
+            }
+        });
+
+        class Range {
+            constructor(rangeNum, condition, inner){
+                this.rangeNum = rangeNum;
+                this.condition = condition;
+                this.inner = inner;
+            }
+            render(){
+                const btn = formItem.querySelector("button");
+                btn.classList.remove("hide");
+                btn.innerHTML = `<i class="fas fa-plus"></i> Добавить ${this.condition.toLowerCase()}`;
+        
+                const rangeWrap = formItem.querySelector(".range-wrapper");
+                const range = document.createElement("div");
+                range.classList.add("range");
+                if(rangeNum > 0){
+                    range.innerHTML = 
+                    `   
+                        <div class="range-title">
+                            <span>или ${this.condition} ${this.rangeNum+1}</span>
+                        </div>
+                        ${this.inner}
+                    `;
+                } else {
+                    range.innerHTML = 
+                    `
+                        <div class="range-title">
+                            <span> ${this.condition} ${this.rangeNum+1}</span>
+                        </div>
+                        ${this.inner}
+                    `;
+                }
+                rangeWrap.append(range);
+                rangeNum++;
+            }
+        }
+
+        function createRange(){
+            new Range(rangeNum, 'Диапазон', range).render();   
+        }
+        function createType(){
+            new Range(rangeNum, 'Тип', type).render();   
+        }
+        function createStatus(){
+            new Range(rangeNum, 'Статус', status1).render();   
+        }
     }
 }
 
-createCondition();
-
-push.addEventListener("click", createCondition);
+new Condition(itemsNum).render();
+push.addEventListener("click", () => new Condition(itemsNum).render());
